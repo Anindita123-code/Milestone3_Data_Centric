9f0d8892733e8ced4ed2206e5e273a9dce65702a
Anindita Som <aninditasom2020@gmail.com>
2021-02-09 11:53:15 +0000

    user reauthentication and profile pages routing

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 @app.route("/")
-@app.route("/home",)
+@app.route("/home")
 def home():
     categories = mongo.db.categories.find()
     return render_template("home.html", categories=categories)
@@ -45,6 +45,7 @@ def register():
 
         session["user"] = request.form.get("username").lower()
         flash("You have been registered successfully !!")
+        return redirect(url_for('login'))
 
     return render_template('register.html')
 
@@ -52,21 +53,49 @@ def register():
 @app.route('/login', methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        user_exists = mongo.db.users.find_one(
+        db_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
-        if user_exists:
+        if db_user:
             if check_password_hash(
-                    user_exists["password"], request.form.get("password")):
+                    db_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
-                flash("Welcome {}".format(request.form.get("username")))
+                flash("Welcome {}".format(request.form.get(
+                    "username").capitalize()))
             else:
                 flash("The Username and/or Password doesnot match!!")
         else:
             flash("The Username and/or Password doesnot match!!")
 
-    return render_template("login.html")
+    if 'user' in session:
+        if session["user"].lower() == "admin":
+            return redirect(url_for('admin_profile'))
+        elif len(session["user"]) >= 5:
+            return redirect(url_for('user_profile', username=session["user"]))
 
+    return render_template('login.html')
+
+
+@app.route("/user_profile/<username>", methods=["GET", "POST"])
+def user_profile(username):
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("user_profile.html", username=username)
+
+
+@app.route("/admin_profile", methods=["GET", "POST"])
+def admin_profile():
+    return render_template("admin_profile.html")
+
+
+@app.route("/logout")
+def logout():
+    if 'user' in session:
+        session.pop('user')
+        flash("You have been logged out!")
+
+    categories = mongo.db.categories.find()
+    return render_template("home.html", categories=categories)
 
 @app.route("/get_categories")
 def get_categories():
